53c8720a6f99bf29eaa5a15ea6dcfec9da296d26
plat67 <jihee.dev@gmail.com>
2021-09-01 00:05:57 +0900

    Time: 370 ms (11.74%), Space: 43.9 MB (17.68%) - LeetHub

--- add-two-numbers/add-two-numbers.kt
+++ add-two-numbers/add-two-numbers.kt
@@ -11,43 +11,31 @@ class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
         var nextNode1 = l1
         var nextNode2 = l2
-        var carry = false
-        var preNode = ListNode(0)
-        var newNode: ListNode
+        var carry = 0
+        var tmpNode = ListNode(0)
+        val resultNode = tmpNode
 
-        val resultNode = preNode
+        while (nextNode1 != null || nextNode2 != null || carry > 0) {
+            var tempSum = carry
+            carry = 0
 
-        while (true) {
-            if (nextNode1 == null && nextNode2 == null) {
-                if(carry) {
-                    newNode = ListNode(1)
-                    preNode.next = newNode
-                    preNode = newNode
-                }
-                break
-            }
-            
-            var tempSum = if (carry) 1 else 0
-            carry = false
-
-            if(nextNode1 != null) {
+            if (nextNode1 != null) {
                 tempSum += nextNode1.`val`
                 nextNode1 = nextNode1.next
             }
 
-            if(nextNode2 != null) {
+            if (nextNode2 != null) {
                 tempSum += nextNode2.`val`
                 nextNode2 = nextNode2.next
             }
 
-            if(tempSum >= 10) {
+            if (tempSum >= 10) {
                 tempSum %= 10
-                carry = true
+                carry = 1
             }
 
-            newNode = ListNode(tempSum)
-            preNode.next = newNode
-            preNode = newNode
+            tmpNode.next = ListNode(tempSum)
+            tmpNode = tmpNode.next!!
         }
 
         return resultNode.next
