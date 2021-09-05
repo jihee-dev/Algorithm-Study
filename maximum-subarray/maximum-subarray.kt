f2c65db15438a3ce4e16e11851f5666ac2671de5
plat67 <jihee.dev@gmail.com>
2021-09-05 20:00:49 +0900

    Time: 156 ms (100.00%), Space: 35.3 MB (86.62%) - LeetHub

--- maximum-subarray/maximum-subarray.kt
+++ maximum-subarray/maximum-subarray.kt
@@ -1,22 +1,13 @@
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
-        var tempSum = 0
-        var max = nums[0]
+        var localMax = 0
+        var globalMax = nums[0]
 
         for (i in nums.indices) {
-            for (j in i until nums.size) {
-                if (i == j) {
-                    tempSum = nums[i]
-                } else {
-                    tempSum += nums[j]
-                }
-
-                if (tempSum > max) {
-                    max = tempSum
-                }
-            }
+            localMax = max(nums[i], nums[i] + localMax)
+            globalMax = max(globalMax, localMax)
         }
 
-        return max
+        return globalMax
     }
 }
